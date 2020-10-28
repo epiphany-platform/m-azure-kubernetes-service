8dc6b5b8f54e8a5a9a40a096790403cf712e7205
atsikham <atsikham@users.noreply.github.com>
2020-10-28 14:04:18 +0100

    Moved module configuration to top level

--- resources/terraform/variables.tf
+++ resources/terraform/variables.tf
@@ -46,5 +46,98 @@ variable "auto_scaling" {
 }
 
 variable "rsa_pub_path" {
-  type = string
+  description = "Public ssh key path"
+  type        = string
+  default     = "/shared/vms_rsa.pub"
+}
+
+# AUTO-SCALER PROFILE
+
+variable "balance_similar_node_groups" {
+  type    = bool
+  default = false
+}
+
+variable "max_graceful_termination_sec" {
+  type    = string
+  default = "600"
+}
+
+variable "scale_down_delay_after_add" {
+  type    = string
+  default = "10m"
+}
+
+variable "scale_down_delay_after_delete" {
+  type    = string
+  default = "10s"
+}
+
+variable "scale_down_delay_after_failure" {
+  type    = string
+  default = "10m"
+}
+
+variable "scan_interval" {
+  type    = string
+  default = "10s"
+}
+
+variable "scale_down_unneeded" {
+  type    = string
+  default = "10m"
+}
+
+variable "scale_down_unready" {
+  type    = string
+  default = "10m"
+}
+
+variable "scale_down_utilization_threshold" {
+  type    = string
+  default = "0.5"
+}
+
+# RANDOM DEFAULTS
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.18.6"
+}
+
+variable "enable_node_public_ip" {
+  type    = bool
+  default = false
+}
+
+variable "default_node_pool_type" {
+  type    = string
+  default = "VirtualMachineScaleSets"
+}
+
+variable "identity_type" {
+  type    = string
+  default = "SystemAssigned"
+}
+
+variable "network_plugin" {
+  type    = string
+  default = "azure"
+}
+
+variable "network_policy" {
+  type    = string
+  default = "azure"
+}
+
+variable "kube_dashboard_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "admin_username" {
+  description = "Admin user on Linux OS"
+  type        = string
+  default     = "operations"
 }
