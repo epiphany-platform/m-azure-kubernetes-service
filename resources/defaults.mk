b7fcac90a351114f56014bac9175135b4f666507
atsikham <atsikham@users.noreply.github.com>
2020-10-30 15:51:58 +0100

    Fixed indents

--- resources/defaults.mk
+++ resources/defaults.mk
@@ -4,26 +4,26 @@ M_VNET_NAME ?= $(M_NAME)-vnet
 M_ADDRESS_PREFIX ?= 10.0.0.0/16
 
 M_DEFAULT_NODE_POOL ?= 	{ \
-							size: 2, \
-							min: 2, \
-							max: 5, \
-							vm_size: Standard_DS2_v2, \
-							disk_size: 36, \
-							auto_scaling: true, \
-							type: VirtualMachineScaleSets \
-						}
+	size: 2, \
+	min: 2, \
+	max: 5, \
+	vm_size: Standard_DS2_v2, \
+	disk_size: 36, \
+	auto_scaling: true, \
+	type: VirtualMachineScaleSets \
+}
 
 M_AUTO_SCALER_PROFILE ?= { \
-							balance_similar_node_groups: false, \
-							max_graceful_termination_sec: 600, \
-							scale_down_delay_after_add: 10m, \
-							scale_down_delay_after_delete: 10s, \
-							scale_down_delay_after_failure: 10m, \
-							scan_interval: 10s, \
-							scale_down_unneeded: 10m, \
-							scale_down_unready: 10m, \
-							scale_down_utilization_threshold: 0.5 \
-						}
+	balance_similar_node_groups: false, \
+	max_graceful_termination_sec: 600, \
+	scale_down_delay_after_add: 10m, \
+	scale_down_delay_after_delete: 10s, \
+	scale_down_delay_after_failure: 10m, \
+	scan_interval: 10s, \
+	scale_down_unneeded: 10m, \
+	scale_down_unready: 10m, \
+	scale_down_utilization_threshold: 0.5 \
+}
 
 # azure credentials
 M_ARM_CLIENT_ID ?= unset
